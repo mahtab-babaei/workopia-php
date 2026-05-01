6750402d6ad8339989deb658b30d315ec2177171
Mahtab <mahtaab.babaei@gmail.com>
2026-05-01 07:56:00 +0330

    add view and partial loader helpers

--- helpers.php
+++ helpers.php
@@ -9,3 +9,37 @@ function basePath($path = '')
 {
   return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * return void
+ */
+function loadView($name) 
+{
+  $viewPath = basePath("views/$name.view.php");
+  
+  if (file_exists($viewPath)) {
+    require $viewPath;
+  } else {
+    echo "file '$name' not founded!";
+  }
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $partial
+ * return void
+ */
+function loadPartial($name) 
+{
+  $partialPath = basePath("views/partials/$name.php");
+
+  if (file_exists($partialPath)) {
+    require $partialPath;
+  } else {
+    echo "file '$name' not founded!";
+  }
+}
